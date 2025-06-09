8723834ec1a699a3ee2cd9e38aa507679a3abd75
Chofa's <sofigmez9@gmail.com>
2025-06-09 14:40:36 -0400

    Se cambio la url a plural

--- src/main/java/com/edutech/classroom/client/CourseClient.java
+++ src/main/java/com/edutech/classroom/client/CourseClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-course", url = "https://localhost:8080/api/course")
+@FeignClient(name = "ms-course", url = "https://localhost:8080/api/courses")
 public interface CourseClient {
     @GetMapping("/{id}")
     Object getCourseById(@PathVariable("id") String id);
--- src/main/java/com/edutech/classroom/controller/CourseController.java
+++ src/main/java/com/edutech/classroom/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/course")
+@RequestMapping("/api/courses")
 @RequiredArgsConstructor
 public class CourseController {
     private final CourseService service;
